58179d9bd4ff88aafd6229fada95b57dc0e9596a
SLCB-VM <161575390+SLCB-VM@users.noreply.github.com>
2025-08-24 21:27:23 +0200

    Update proxy.js

--- functions/proxy.js
+++ functions/proxy.js
@@ -9,15 +9,17 @@ export async function handler(event) {
     const arrayBuffer = await res.arrayBuffer();
     const base64File = Buffer.from(arrayBuffer).toString("base64");
 
+    const fileName = fileUrl.split("/").pop() || "firmware.bin";
+
     return {
       statusCode: 200,
       headers: {
         "Content-Type": res.headers.get("content-type") || "application/octet-stream",
-        "Content-Disposition": `attachment; filename="${fileUrl.split("/").pop()}"`,
+        "Content-Disposition": `attachment; filename="${fileName}"`,
         "Cache-Control": "no-cache",
       },
       body: base64File,
-      isBase64Encoded: true, // important!
+      isBase64Encoded: true,
     };
   } catch (err) {
     return { statusCode: 500, body: err.toString() };
